1740b2a63e47a88ab9f472f0cfb572ac296840d1
Ngoc Linh Ta <tangoclinh1995@gmail.com>
2016-11-11 00:31:22 +0800

    Add code in app.js to monitor user state change

--- www/app.js
+++ www/app.js
@@ -1,6 +1,6 @@
 angular.module("thehonorclub", ["ionic", "ionic-datepicker", "firebase"])
 
-.run(function($ionicPlatform) {
+.run(function($ionicPlatform, $rootScope, $state) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -14,10 +14,42 @@ angular.module("thehonorclub", ["ionic", "ionic-datepicker", "firebase"])
       StatusBar.styleDefault();
     }
   });
+
+  // Monitor authentication state, if detect user has signed out, then automatically
+  // redirect to Signin page
+  firebase.auth().onAuthStateChanged(function(user) {
+    // Null user object means "Signed Out"
+    if (user == null) {
+      $state.go("signin");
+    }
+
+  });
+
+  // Monitor Angular UI Router state change, if detect that a non-Sign-in page is
+  // being directed to without user signing in, then force redirecting back to
+  // Signin page
+  $rootScope.$on("$stateChangeStart", function(even, toState) {
+    // Don't care if upcoming state is Signin
+    if (toState.name == "signin") {
+      return;
+    }
+
+    // Otherwise, check whether user has been signed in
+    if (firebase.auth().currentUser == null) {
+      // TRICK: Wait a litte bit before redirection to make sure
+      // redirection successfully occurs
+      setTimeout(function() {
+        $state.go("signin");
+      }, 100);
+
+    }
+
+  }); 
+
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
   // Default state
-  $urlRouterProvider.otherwise('/login');
+  $urlRouterProvider.otherwise('/signin');
 
 });
