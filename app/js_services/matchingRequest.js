9c80b96f9e6d26885436508d548df5b82c3c3a05
Ngoc Linh Ta <tangoclinh1995@gmail.com>
2016-11-11 18:46:36 +0800

    Add inviteMember and acceptMatch to matchingRequest

--- app/js_services/matchingRequest.js
+++ app/js_services/matchingRequest.js
@@ -5,14 +5,14 @@ angular.module("thehonorclub")
 
 
 
-  function joinTeamRequest(from_user_uid, to_team_uid) {
+  function joinTeamRequest(fromUserUid, toTeamUid) {
     var defer = $q.defer();
     
     // This is used to check if a similar Team-Joining Request has already been issue
     var queryExist =
       dbRefJoinTeam
-      .equalTo(from_user_uid, "from_user_uid")
-      .equalTo(to_team_uid, "to_team_uid")
+      .equalTo(fromUserUid, "from_user_uid")
+      .equalTo(toTeamUid, "to_team_uid")
       .limitToFirst(1);
 
     // This is used to check if a Member-Invitation Request (with similar team uid, user uid)
@@ -20,8 +20,8 @@ angular.module("thehonorclub")
     // If there is, then IT'S A MATCH
     var reverseQueryExist =
       dbRefInviteMember
-      .equalTo(to_team_uid, "from_team_uid")
-      .equalTo(from_user_uid, "to_member_uid")
+      .equalTo(toTeamUid, "from_team_uid")
+      .equalTo(fromUserUid, "to_member_uid")
       .limitToFirst(1);
 
     queryExist.once("value")
@@ -36,7 +36,7 @@ angular.module("thehonorclub")
       // Now check whether a reverse request has already exist
       // (find if THERE IS A MATCH)
       // Chain this to NEXT THEN
-      return reverseQueryExist.once("value")
+      return reverseQueryExist.once("value");
 
     })
     .then(function(dataSnapshot) {
@@ -49,8 +49,8 @@ angular.module("thehonorclub")
       // Otherwise, add a new Team-Joining request
       // Chain this to NEXT THEN
       return dbRefJoinTeam.push({
-        from_user_uid: from_user_uid,
-        to_team_uid: to_team_uid
+        from_user_uid: fromUserUid,
+        to_team_uid: toTeamUid
       });
 
     })
@@ -63,6 +63,139 @@ angular.module("thehonorclub")
     });
 
     return defer.promise;
+  };
+
+
+
+  // This function is just a reverse of joinTeamRequest function
+  function inviteMember(fromTeamUid, toMemberUid) {
+    var defer = $q.defer();
+    
+    var queryExist =
+      dbRefInviteMember
+      .equalTo(fromTeamUid, "from_team_uid")
+      .equalTo(toMemberUid, "to_member_uid")
+      .limitToFirst(1);
+
+    var reverseQueryExist =
+      dbRefJoinTeam
+      .equalTo(toMemberUid, "from_user_uid")
+      .equalTo(fromTeamUid, "to_team_uid")
+      .limitToFirst(1);
+
+    queryExist.once("value")
+    .then(function(dataSnapshot) {
+      if (dataSnapshot.numChildren() != 0) {
+        defer.resolve(false);
+        return;
+      }
+
+      return reverseQueryExist.once("value");
+
+    })
+    .then(function(dataSnapshot) {
+      // IT'S A MATCH
+      if (dataSnapshot.numChildren() != 0) {
+        defer.resolve(true);
+        return;
+      };
+
+      // Otherwise, add a new Member-Invitation request
+      // Chain this to NEXT THEN
+      return dbRefInviteMember.push({
+        from_team_uid: fromTeamUid,
+        to_member_uid: toMemberUid
+      });
+
+    })
+    .then(function() {
+      defer.resolve(false);
+      
+    })
+    .catch(function() {
+      defer.reject();
+    });
+
+    return defer.promise;
+  };
+
+
+  // Process the success matching between team & member
+  function acceptMatch(teamUid, userUid) {
+    var defer = $q.defer();
+
+    var jobRemaining = 4;
+    function done() {
+      --jobRemaining;
+      
+      if (jobRemaining == 0) {
+        defer.resolve();
+      };
+
+    };  
+
+    // Delete Team-Joing Request
+    dbRefJoinTeam
+    .equalTo(userUid, "from_user_uid")
+    .equalTo(teamUid, "to_team_uid")
+    .once("value")
+    .then(function(dataSnapshot) {
+
+      dataSnapshot.forEach(function(child) {
+        child.ref.remove();
+      });
+
+      done();
+
+    })
+    .catch(function() {
+      defer.reject();
+    });
+
+    // Delete Member-Inviting Request
+    dbRefInviteMember
+    .equalTo(teamUid, "from_team_uid")
+    .equalTo(memberUid, "to_member_uid")
+    .once("value")    
+    .then(function(dataSnapshot) {
+      
+      dataSnapshot.forEach(function(child) {
+        child.ref.remove();
+      });
+
+      done();
+
+    })
+    .catch(function() {
+      defer.reject();
+    })
+
+    // Add user to team
+    firebase.database().ref()
+    .child("team").child(teamUid).child("members_uid")
+    .push(userUid)
+    .then(function() {
+      done();
+
+    })
+    .catch(function() {
+      defer.reject();
+    });
+
+    // Add team to user
+    firebase.database().ref()
+    .child("user_info").child(userUid).child("member_of")
+    .push(teamUid)
+    .then(function() {
+      done();
+
+    })
+    .catch(function() {
+      defer.reject();
+    });
+    
+
+    return defer.promise();
   };
 
 
