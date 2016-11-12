d718db214c649569b63c5a9bb6af7004e68ea22d
Jay Shin <jshin49@gmail.com>
2016-11-12 20:50:45 +0800

    Add Landing Page HTMLs

---
+++ Landing_Page/js/controllers.js
@@ -0,0 +1,10 @@
+angular.module('app.controllers', [])
+  
+.controller('page1Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
