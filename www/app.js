8e9cfbdd5bcdecb8fe3f96e23c1d25a2a8252d3a
Jenny Kang <jennykang95@gmail.com>
2016-11-14 17:06:43 +0800

    initial commit for card ui

--- www/app.js
+++ www/app.js
@@ -1,4 +1,4 @@
-angular.module("thehonorclub", ["ionic", "ionic-datepicker", "firebase"])
+angular.module("thehonorclub", ["ionic", "ionic-datepicker", "firebase", "ionic.contrib.ui.tinderCards"])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -14,10 +14,47 @@ angular.module("thehonorclub", ["ionic", "ionic-datepicker", "firebase"])
       StatusBar.styleDefault();
     }
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
+}).config(function($stateProvider, $urlRouterProvider) {
   // Default state
   $urlRouterProvider.otherwise('/login');
 
-});
+}).directive('noScroll', function() {
+
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+
+      $element.on('touchmove', function(e) {
+        e.preventDefault();
+      });
+    }
+  }
+}).controller('CardsCtrl', function($scope, TDCardDelegate) {
+  var cardTypes = [
+    { image: 'http://ionic-forum-static.s3.amazonaws.com/tinder/max.jpg' },
+    { image: 'http://ionic-forum-static.s3.amazonaws.com/tinder/ben.png' },
+    { image: 'http://ionic-forum-static.s3.amazonaws.com/tinder/perry.jpg' },
+  ];
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.unshift(angular.extend({}, newCard));
+  }
+  
+  $scope.cards = [];
+  for(var i = 0; i < 3; i++) $scope.addCard();
+}).controller('CardCtrl', function($scope, TDCardDelegate) {
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
+});;
