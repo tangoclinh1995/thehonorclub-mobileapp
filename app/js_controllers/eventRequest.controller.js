ea82dac6f0e3808a1c0c2c77d255ec537e7838a6
Ngoc Linh Ta <tangoclinh1995@gmail.com>
2016-11-14 21:37:09 +0800

    Fix some dramatic accidents

--- app/js_controllers/eventRequest.controller.js
+++ app/js_controllers/eventRequest.controller.js
@@ -1,4 +1,5 @@
 angular.module("thehonorclub") 
+
 .controller('EventRequestCtrl', function($scope, ionicTimePicker, ionicDatePicker) {
 
   var ref = firebase.database().ref().child("event");
@@ -106,4 +107,37 @@ angular.module("thehonorclub")
     });
 
   };
+})
+
+.config(function(ionicDatePickerProvider, ionicTimePickerProvider) {
+  // Configure ionic datepicker
+  var datePickerObj = {
+    inputDate: new Date(),
+    titleLabel: 'Select a Date',
+    setLabel: 'Set',
+    todayLabel: 'Today',
+    closeLabel: 'Close',
+    mondayFirst: false,
+    weeksList: ["S", "M", "T", "W", "T", "F", "S"],
+    monthsList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+    templateType: 'popup',
+    from: new Date(2016, 8, 1),
+    to: new Date(2020, 8, 1),
+    showTodayButton: true,
+    dateFormat: 'dd MMMM yyyy',
+    closeOnSelect: false,
+    disableWeekdays: []
+  };
+  ionicDatePickerProvider.configDatePicker(datePickerObj);
+
+  // Configure ionic timepicker
+  var timePickerObj = {
+    inputTime: (((new Date()).getHours() * 60 * 60) + ((new Date()).getMinutes() * 60)),
+    format: 12,
+    step: 15,
+    setLabel: 'Set',
+    closeLabel: 'Close'
+  };
+  ionicTimePickerProvider.configTimePicker(timePickerObj);
+
 });
