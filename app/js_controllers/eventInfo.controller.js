abbbf5a1e7cb1dadd30ea7e48d19d6a5abd02b94
Ashish <sleeplessclassics@gmail.com>
2016-11-16 03:13:22 +0800

    Added basic functionality to the event page

--- app/js_controllers/eventInfo.controller.js
+++ app/js_controllers/eventInfo.controller.js
@@ -2,7 +2,42 @@ angular.module("thehonorclub")
 
 .controller('EventRequestCtrl', function($scope, $cordovaSocialSharing) {
     
-  var message = "Hey, checkout this event" + event.name + ".";
+  //Get Firebase event
+  function getFirebaseEvent() {}
+  var firebaseEvent = getFirebaseEvent();
+  
+  // Convert FireBase Event to event (Functions and variables)
+  var event = {};
+  event.name = firebaseEvent.name;
+  event.description = firebaseEvent.description;
+  event.location = firebaseEvent.location;
+  event.email = firebaseEvent.email;
+  event.min_team_size = firebaseEvent.min_member_per_team;
+  event.max_team_size = firebaseEvent.max_member_per_team;
+  event.start_date = getStartDate();
+  event.start_time = getStartTime();
+  event.end_date = getEndDate();
+  event.end_time = getEndTime();
+
+  var getStartDate= function() {
+    return moment.unix(firebaseEvent.timestamp_begin).format("DD/MM/YYYY");
+  }
+
+  var getStartTime = function() {
+    return moment.unix(firebaseEvent.timestamp_begin).format("hh:mm a");
+  }
+
+  function getEndDate() {
+    return moment.unix(firebaseEvent.timestamp_end).format("DD/MM/YYYY");
+  }
+
+  function getEndTime() {
+    return moment.unix(firebaseEvent.timestamp_end).format("hh:mm a");
+  }
+
+
+  // Social Sharing
+  var message = "Hey, checkout this event" + event.name + "."; 
 
   $scope.shareTwitter = function() {
     $cordovaSocialSharing.canShareVia("twitter", message).then(function(result) {
