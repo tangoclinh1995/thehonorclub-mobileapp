6e98a0b679510236cf5f921b06beb520cd1a4f40
Jay Shin <jshin49@gmail.com>
2016-11-16 02:38:34 +0800

    Remove login.service.js

--- app/js_services/login.service.js
+++
@@ -1,9 +0,0 @@
-angular.module("thehonorclub")
-.factory("Auth", function(firebaseKey) {
-
-	console.log(firebaseKey);
-	var auth = JSON.parse(localStorage.getItem("firebase:authUser:"+firebaseKey+":[DEFAULT]"));
-	console.log(auth);
-
-	return auth;
-});
