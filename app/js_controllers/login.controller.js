c435be0e0474441713bd74fcc1e33eb90000d4df
Jay Shin <jshin49@gmail.com>
2016-11-15 15:17:51 +0800

    Fix signInWithRedirect is buggy so defaulted to signInWithPopup

--- app/js_controllers/login.controller.js
+++ app/js_controllers/login.controller.js
@@ -14,37 +14,65 @@ angular.module('thehonorclub')
 	// console.log($firebaseAuthInstance.auth._._auth.currentUser);
   // firebase.database().ref().push(firebase.auth().currentUser.providerData);
 
-  console.log(Auth.provider);
+  console.log(Auth.auth);
 
 	$scope.login = function() {
 		var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+		  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+		  var token = result.credential.accessToken;
 
-    firebase.auth().signInWithRedirect(provider);
-		firebase.auth().getRedirectResult().then(function(authData) {
-	    if (authData === null) {
-	      console.log('Not logged in yet');
-	    } 
-	    else {
-	    	// console.log(authData);
-	      // console.log('Logged in as', authData.user);
-	      firebase.database().ref('users/'+authData.user.providerData[0].uid).set(authData.user.providerData[0]);
-	    	$state.go('userprofile');
-	      // firebase.database().ref().push(firebase.auth().currentUser.providerData);
-		    // This will display the user's name in our view
-		    // $firebaseAuthInstance.authData = authData.providerData;
-	    	// $scope.user = authData.user.providerData[0];
-	    }
+		  // The signed-in user info.
+		  var user = result.user;
+      firebase.database().ref('users/'+user.providerData[0].uid).set(user.providerData[0]);
+  		$state.go('userprofile');
+
 		})
 		.catch(function(error) {
-			if (error.code === 'TRANSPORT_UNAVAILABLE') {
-      	firebase.auth().signInWithPopup(provider).then(function(authData) {
-					console.log(authData);
-      	});
-      } 
-      else {
-      	console.error(error);
-  		}
-    });
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  console.error(errorMessage);
+		  // The email of the user's account used.
+		  var email = error.email;
+		  // The firebase.auth.AuthCredential type that was used.
+		  var credential = error.credential;
+		  // ...
+		});
+
+   //  firebase.auth().signInWithRedirect(provider).then(()=>{
+   //  	console.log("Hello");
+			// firebase.auth().getRedirectResult().then(function(authData) {
+			// 	console.log("Hello2");
+		 //    if (authData === null) {
+		 //      console.log('Not logged in yet');
+		 //    } 
+		 //    else {
+		 //    	// console.log(authData);
+		 //      // console.log('Logged in as', authData.user);
+		 //      firebase.database().ref('users/'+firebase.auth().currentUser.providerData[0].uid).set(firebase.auth().currentUser.providerData[0])
+		 //      .then(()=> {
+		 //      	console.log("Pushed");
+		 //    		$state.go('userprofile');
+		 //      });
+		 //      // firebase.database().ref('users/'+authData.user.providerData[0].uid).set(authData.user.providerData[0]);
+			//     // This will display the user's name in our view
+			//     // $firebaseAuthInstance.authData = authData.providerData;
+		 //    	// $scope.user = authData.user.providerData[0];
+		 //    }
+			// })
+			// .catch(function(error) {
+			// 	if (error.code === 'TRANSPORT_UNAVAILABLE') {
+	  //     	firebase.auth().signInWithPopup(provider).then(function(authData) {
+			// 			console.log(authData);
+	  //     	});
+	  //     } 
+	  //     else {
+	  //     	console.error(error);
+	  // 		}
+	  //   });
+   //  })
+		// 
   };
 
 }]);
