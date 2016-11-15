205e51961e11b80bef72c3242315c05024971781
Sharat Madhusudanan <svm@Sharats-MacBook-Pro.local>
2016-11-15 22:02:49 +0800

    Created unit test (incomplete at this time)

---
+++ unit_testing/test_controllers/cardController.spec.js
@@ -0,0 +1,86 @@
+describe("cardController", function () {
+
+    beforeEach(module('thehonorclub'));
+
+    describe('Testing the Directive', function () {
+
+      var $compile, $rootScope;
+      var element, directve;
+
+      beforeEach(inject(function(_$compile_, _$rootScope_) {
+
+        compile = _$compile_;
+        $rootScope = _$rootScope_;
+        element = 'touchmove';
+        directve = $directive('noScroll', {$rootScope:rootScope});
+
+      }));
+
+      it('should set preventDefault on', function () {
+          expect(element.preventDefault()).toBeTruthy();
+      });
+
+    });
+
+    describe('Testing the Controller', function () {
+
+      var scope, ctrlr;
+
+      beforeEach(inject(function($controller,$rootscope) {
+
+        scope = $rootscope.$new();
+        controller = $controller('CardController')
+
+      }));
+
+    });
+
+});
+
+-------
+
+angular.module('thehonorclub')
+
+.directive('noScroll', function() {
+
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+
+      $element.on('touchmove', function(e) {
+        e.preventDefault();
+      });
+    }
+  }
+})
+
+.controller('CardController', function($scope, TDCardDelegate) {
+  var cardTypes = [
+    { image: 'http://ionic-forum-static.s3.amazonaws.com/tinder/max.jpg', name: 'Andy', skill: 'AngularJS' },
+    { image: 'http://ionic-forum-static.s3.amazonaws.com/tinder/ben.png', name: 'Bob', skill: 'NodeJS' },
+    { image: 'http://ionic-forum-static.s3.amazonaws.com/tinder/perry.jpg', name: 'Charlie', skill: 'Ionic' },
+  ];
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.unshift(angular.extend({}, newCard));
+  }
+
+  $scope.cards = [];
+  for(var i = 0; i < 3; i++) $scope.addCard();
+}).controller('CardCtrl', function($scope, TDCardDelegate) {
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
+  
+});
