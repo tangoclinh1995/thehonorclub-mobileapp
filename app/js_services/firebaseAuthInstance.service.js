6237b786f8bc6dcd1ba5cc5d2c13c3ef03d012a5
Jay Shin <jshin49@gmail.com>
2016-11-16 02:35:12 +0800

    Modified firebaseAuthInstance to fetch user data from localStorage, same API

--- app/js_services/firebaseAuthInstance.service.js
+++ app/js_services/firebaseAuthInstance.service.js
@@ -1,4 +1,9 @@
 angular.module("thehonorclub")
-.factory("$firebaseAuthInstance", function($firebaseAuth) {
-  return $firebaseAuth();
+.factory("$firebaseAuthInstance", function(firebaseKey) {
+	var factory = {};	console.log(firebaseKey);
+	var auth = JSON.parse(localStorage.getItem("firebase:authUser:"+firebaseKey+":[DEFAULT]"));
+	console.log(auth);
+	factory.getAuth() = () => { return auth; };
+
+	return factory;
 });
