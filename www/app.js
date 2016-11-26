aa95b3a8c9c1e43d78418ac57c5885bb8714f88c
Jay Shin <jshin49@gmail.com>
2016-11-26 23:48:55 +0800

    Added state monitor

--- www/app.js
+++ www/app.js
@@ -3,7 +3,7 @@ angular.module(
   ["ionic", "ionic-datepicker", "ionic-timepicker", "firebase", "ionic.contrib.ui.tinderCards", "ngCordova"]
 )
 
-.run(function($ionicPlatform, $rootScope, $state) {
+.run(function($ionicPlatform, $rootScope, $state, $ionicNavBarDelegate) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -49,6 +49,16 @@ angular.module(
 
   // }); 
 
+  // Monitor state chage to allow back button or not
+  $rootScope.$on('$stateChangeSuccess', function (ev, to, toParams, from, fromParams) {
+    console.log(from);
+    if (from.name === 'loading' || from.name === 'login') {
+      $ionicNavBarDelegate.showBackButton(false);
+    }
+    else {
+      $ionicNavBarDelegate.showBackButton(true);
+    }
+  });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
