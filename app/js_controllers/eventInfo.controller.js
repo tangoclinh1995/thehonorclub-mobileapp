70cb7314b880e98b731cc140239353f697567394
Ngoc Linh Ta <tangoclinh1995@gmail.com>
2016-11-28 18:32:49 +0800

    Improve the current event info page

--- app/js_controllers/eventInfo.controller.js
+++ app/js_controllers/eventInfo.controller.js
@@ -1,70 +1,69 @@
 angular.module("thehonorclub") 
 
-.controller('EventInfoCtrl', function($scope, $cordovaSocialSharing, $stateParams) {
-    
-  //Get Firebase event
-  function getFirebaseEvent() {
-    var databaseRef = firebase.database().ref();
-    var currentUser = $firebaseAuthInstance.$getAuth();
-    return $firebaseObject(databaseRef.child("event").child($stateParams.eventUid));
-  }
-  var firebaseEvent = getFirebaseEvent();
+.controller('EventInfoCtrl', function($scope, $stateParams, $firebaseAuthInstance, $firebaseObject, $cordovaSocialSharing) {
+  var databaseRef = firebase.database().ref(); 
   
-  // Convert FireBase Event to event (Functions and variables)
-  var event = {};
-  event.name = firebaseEvent.name;
-  event.description = firebaseEvent.description;
-  event.location = firebaseEvent.location;
-  event.email = firebaseEvent.email;
-  event.min_team_size = firebaseEvent.min_member_per_team;
-  event.max_team_size = firebaseEvent.max_member_per_team;
-  event.start_date = getStartDate();
-  event.start_time = getStartTime();
-  event.end_date = getEndDate();
-  event.end_time = getEndTime();
+  $scope.event = $firebaseObject(databaseRef.child("event").child($stateParams.eventUid));
 
-  var getStartDate= function() {
-    return moment.unix(firebaseEvent.timestamp_begin).format("DD/MM/YYYY");
+  $scope.unixTimestampToString = function(timestamp) {
+    return moment.unix(timestamp).format("DD/MM/YYYY HH:mm");
   }
 
-  var getStartTime = function() {
-    return moment.unix(firebaseEvent.timestamp_begin).format("hh:mm a");
-  }
-
-  function getEndDate() {
-    return moment.unix(firebaseEvent.timestamp_end).format("DD/MM/YYYY");
-  }
-
-  function getEndTime() {
-    return moment.unix(firebaseEvent.timestamp_end).format("hh:mm a");
-  }
-
-
-  // Social Sharing
-  var message = "Hey, checkout this event" + event.name + "."; 
+  var message = "";
+  $scope.event.$loaded().then(function() {
+    message = "Hey, checkout this event" + $scope.event.name + ".";
+  }); 
 
   $scope.shareTwitter = function() {
-    $cordovaSocialSharing.canShareVia("twitter", message).then(function(result) {
-      $cordovaSocialSharing.shareViaTwitter(message);
-    }, function(error) { alert("Cannot share on Twitter");});
+    $cordovaSocialSharing.canShareVia("twitter", message).then(
+      function(result) {
+        $cordovaSocialSharing.shareViaTwitter(message);
+      },
+      function(error) {
+        alert("Cannot share on Twitter");
+      }
+
+    );
+
   }
 
   $scope.shareInstagram = function() {
-    $cordovaSocialSharing.canShareVia("instagram", message).then(function(result) {
-      $cordovaSocialSharing.shareViaInstagram(message);
-    }, function(error) { alert("Cannot share on Instagram");});
+    $cordovaSocialSharing.canShareVia("instagram", message).then(
+      function(result) {
+        $cordovaSocialSharing.shareViaInstagram(message);
+      },
+      function(error) {
+        alert("Cannot share on Instagram");
+      }
+
+    );
+
   }
 
   $scope.shareWhatsApp = function() {
-    $cordovaSocialSharing.canShareVia("whatsapp", message).then(function(result) {
-      $cordovaSocialSharing.shareViaWhatsApp(message);
-    }, function(error) { alert("Cannot share on WhatsApp");});
+    $cordovaSocialSharing.canShareVia("whatsapp", message).then(
+      function(result) {
+        $cordovaSocialSharing.shareViaWhatsApp(message);
+      },
+      function(error) {
+        alert("Cannot share on WhatsApp");
+      }
+
+    );
+    
   }
   
   $scope.shareFacebook = function() {
-    $cordovaSocialSharing.canShareVia("facebook", message).then(function(result) {
-      $cordovaSocialSharing.shareViaFacebook(message);
-    }, function(error) { alert("Cannot share on Facebook");});
+    $cordovaSocialSharing.canShareVia("facebook", message).then(
+      function(result) {
+        $cordovaSocialSharing.shareViaFacebook(message);
+      },
+      function(error) {
+        alert("Cannot share on Facebook");
+      }
+      
+    );
+
   }
 
 });
