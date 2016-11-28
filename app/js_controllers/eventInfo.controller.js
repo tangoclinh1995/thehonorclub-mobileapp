0a690a26bfc37fc2e0cb2425580366be53f174ac
Ashish <sleeplessclassics@gmail.com>
2016-11-28 17:43:37 +0800

    Mobile App-Event Commits

--- app/js_controllers/eventInfo.controller.js
+++ app/js_controllers/eventInfo.controller.js
@@ -1,6 +1,6 @@
 angular.module("thehonorclub") 
 
-.controller('EventInfoCtrl', function($scope, $cordovaSocialSharing, $stateParams) {
+.controller('EventInfoCtrl', function($scope, $stateParams, $http) {
     
   //Get Firebase event
   function getFirebaseEvent() {
@@ -12,16 +12,6 @@ angular.module("thehonorclub")
   
   // Convert FireBase Event to event (Functions and variables)
   var event = {};
-  event.name = firebaseEvent.name;
-  event.description = firebaseEvent.description;
-  event.location = firebaseEvent.location;
-  event.email = firebaseEvent.email;
-  event.min_team_size = firebaseEvent.min_member_per_team;
-  event.max_team_size = firebaseEvent.max_member_per_team;
-  event.start_date = getStartDate();
-  event.start_time = getStartTime();
-  event.end_date = getEndDate();
-  event.end_time = getEndTime();
 
   var getStartDate= function() {
     return moment.unix(firebaseEvent.timestamp_begin).format("DD/MM/YYYY");
@@ -39,6 +29,16 @@ angular.module("thehonorclub")
     return moment.unix(firebaseEvent.timestamp_end).format("hh:mm a");
   }
 
+  event.name = firebaseEvent.name;
+  event.description = firebaseEvent.description;
+  event.location = firebaseEvent.location;
+  event.email = firebaseEvent.email;
+  event.min_team_size = firebaseEvent.min_member_per_team;
+  event.max_team_size = firebaseEvent.max_member_per_team;
+  event.start_date = getStartDate();
+  event.start_time = getStartTime();
+  event.end_date = getEndDate();
+  event.end_time = getEndTime();
 
   // Social Sharing
   var message = "Hey, checkout this event" + event.name + "."; 
@@ -66,5 +66,36 @@ angular.module("thehonorclub")
       $cordovaSocialSharing.shareViaFacebook(message);
     }, function(error) { alert("Cannot share on Facebook");});
   }
+
+  //Google Location Map
+  var address = event.location;
+  // Get the latitude and longitude
+  $scope.latitude, $scope.longitude;
+  $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=AIzaSyDxeIXJA3pdyq8mwVUla-Kcva7e8LHDo3k')
+    .then(function(_results){
+      $scope.latitude = _results.data.results[0].geometry.location.lat;
+      $scope.longitude = _results.data.results[0].geometry.location.lng;
+     }, 
+     function error(_error){
+       $scope.latitude = 22.3364;
+       $scope.longitude = 114.2655;
+     });
+
+  // Adding the location to the Map
+  $scope.map;
+  $scope.mapLocation = {lat: $scope.latitude, lng: $scope.longitude};
+  function initMap() {
+    $scope.map = new google.maps.Map($scope.map, {
+      center: $scope.mapLocation,
+      zoom: 15
+    });
+  }
+  
+  // Adding the marker to the location
+  $scope.marker;
+  $scope.marker = new google.maps.Marker({ 
+    position: $scope.mapLocation,
+    map: $scope.map
+  });
 
 });
